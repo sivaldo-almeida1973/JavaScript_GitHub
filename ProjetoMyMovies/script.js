f743cedcb1b03805e174efc2f890806b1315072c
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-09-25 09:31:47 -0300

    Capturando as entradas do usuário

--- ProjetoMyMovies/script.js
+++ ProjetoMyMovies/script.js
@@ -1,9 +1,13 @@
 const $searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
+const movieName = document.getElementById("movie-name")
+const movieYear = document.getElementById("movie-year")
 
 //funcao que vai gerenciar o click no tutton de busca
 function $searchButtonClickHandler() {
   overlay.classList.add('open');
+  console.log(movieName.value.split(' ').join('+'));
+  console.log('Ano: ', movieYear.value);
 }
 
 //chama a funcao com o click no button
