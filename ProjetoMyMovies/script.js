1919c78b312084e6e519e4ce9f872b8b075cee67
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-09-25 13:01:52 -0300

    Lidando com erros

--- ProjetoMyMovies/script.js
+++ ProjetoMyMovies/script.js
@@ -5,15 +5,40 @@ const movieYear = document.getElementById("movie-year")
 
 //funcao que vai gerenciar o click no tutton de busca
 async function searchButtonClickHandler() {
-  let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName.value.split(' ').join('+')}&y=${movieYear.value}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("data", data);
-  overlay.classList.add('open');
+  try {
+    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}&y=${ movieYearParameterGenarator()}`;
+  
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data", data);
+    overlay.classList.add('open');
 
-  // console.log(movieName.value.split(' ').join('+'));
-  // console.log('Ano: ', movieYear.value);
+  }catch (error) {
+    console.error(error.message);
+
+  }
+ 
 }
+
+
+function movieNameParameterGenerator() {
+  if(movieName.value === '') {
+    throw new Error('O nome do filme deve ser informado');
+  }
+  return movieName.value.split(' ').join('+');
+}
+
+
+function movieYearParameterGenarator() {
+  if (movieYear.value === '') {
+    return '';
+  }
+  if (movieYear.value.lenght !== 4 || Number.isNaN(Number(movieYear.value))) {
+    throw new Error('Ano do filme inválido.');
+  }
+  return `&y=${movieYear.value}`;
+}
+
 
 //chama a funcao com o click no button
 searchButton.addEventListener("click", searchButtonClickHandler);
