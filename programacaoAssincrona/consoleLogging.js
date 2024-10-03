144e0c1631b82c4d05f01f33a78da057d2bddfba
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-10-03 11:45:10 -0300

    funcoes  Callbacks

--- programacaoAssincrona/consoleLogging.js
+++ programacaoAssincrona/consoleLogging.js
@@ -1,11 +1,33 @@
+// console.log(1);
+
+// //funcao assincrona
+// const result =
+//  setTimeout(() => write2OnConsoleAndReturnString(console.log), 2000);
+
+
+
+// function write2OnConsoleAndReturnString(callbackFunction) {
+//   console.log(2);
+//   callbackFunction('Procedure Completed');
+  
+// }
+
+
+//  passando um funcao anonima como parametro
+
 console.log(1);
 
 //funcao assincrona
-const result = setTimeout(write2OnConsoleAndReturnString, 2000);
-console.log(result);
+const result =
+ setTimeout(() => write2OnConsoleAndReturnString((message) => {
+  console.log(message);
+  console.log(message + "mensagem escrita no console por uma segunda vez!")
+ }), 2000);
 
 
-function write2OnConsoleAndReturnString() {
+
+function write2OnConsoleAndReturnString(callbackFunction) {
   console.log(2);
-  return 'Procedure Completed'
+  callbackFunction('Procedure Completed');
+  
 }
