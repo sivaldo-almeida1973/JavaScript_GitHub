38972f70610994f3c588f5fbcf5c98c94dc44530
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-10-13 21:48:05 -0300

    Mudando a estrutura com Arrow Functions

--- funcoesJS/functionStructure.js.js
+++ funcoesJS/functionStructure.js.js
@@ -1,28 +1,66 @@
-function writeMessageOnConsoleAndReturnTwo(message) {
-  console.log(message);
-  return 2;
-}
+// function writeMessageOnConsoleAndReturnTwo(message) {
+//   console.log(message);
+//   return 2;
+// }
 
-//palavra-chave function =>
+//palavra-chave function =X
 //nome da funcao X
-//parênteses = parâmentros =>
-//bloco  de chaves com instruções dentro =>
+//parênteses = parâmentros => V necessario
+//bloco  de chaves com instruções dentro => V necessario
 //instrução de retorno  V
 
 
-//palvra function pode não existir , na funcao anonima, para issso usar varivel para a funcao
+//palavra function pode não existir , na funcao anonima, para issso usar varivel para a funcao
 const variableThatStoreFunctio = function(message) {
   console.log(message);
   return undefined
 }
 
 //o retorna da funcao por padrão sempre será undefined
-const variableThatStoreFunctio2 = function(message) {
-  console.log(message);
+// const variableThatStoreFunctio2 = function(message) {
+//   console.log(message);
 
+// }
+
+
+// console.log(`O retorno da fução na primeira variável é ${variableThatStoreFunctio('função da primeira variável!')}`);
+
+// console.log(`O retorno da fução na segunda variável é ${variableThatStoreFunctio('função da segunda variável!')}`);
+
+
+// arrow function com um parametro não precisa ter ()
+// com 2 parametros ou sem parametros obrigado ter ()
+
+const writeMessageOnConsoleAndReturnTwo = message => {
+  console.log(message);
+  return 2;
 }
 
 
-console.log(`O retorno da fução na primeira variável é ${variableThatStoreFunctio('função da primeira variável!')}`);
+//funcao padrao
 
-console.log(`O retorno da fução na segunda variável é ${variableThatStoreFunctio('função da segunda variável!')}`);
+function squareNamber(originaNamber) {
+  return originaNamber * originaNamber;
+}
+
+//mesma funcao  de cima , mas como ( arrow function)
+const squareNamberArrow1 = originaNamber => {
+  return originaNamber * originaNamber;
+}
+
+//(arrow function) pode ser escrita assim , quando tiver apenas uma instrucao
+const squareNamberArrow = originaNamber => originaNamber * originaNamber;
+
+
+console.log(`2 ao quadrado vale ${squareNamber(2)}`);
+console.log(`3 ao quadrado vale ${squareNamberArrow1(3)}`);
+console.log(`2 ao quadrado vale ${squareNamberArrow(2)}`);
+
+
+
+const arrowFunctionQueRetornaObjeto = () => {
+  return {
+    id: 0,
+    nome: 'Sivaldo vieira',
+  }
+}
