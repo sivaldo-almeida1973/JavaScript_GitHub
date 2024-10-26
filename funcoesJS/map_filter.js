14a556b3bbaf907bf6cf4092148b4c5d872600da
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-10-26 11:45:16 -0300

    Reduce

--- funcoesJS/map_filter.js
+++ funcoesJS/map_filter.js
@@ -1,4 +1,4 @@
-const faturamentoEmDolar = [
+const faturamento = [
   150_000, 110_000, 90_000, 70_000, 100_000, 120_000, 130_000, 95_000,120_000,130_000,120_000,165_000];
 
 
@@ -6,7 +6,7 @@ const faturamentoEmDolar = [
 
   //map
 
-const faturamentoEmReais = faturamentoEmDolar.map(
+const faturamentoEmReais = faturamento.map(
   (valorFaturamentoMensal) => 4.79 * valorFaturamentoMensal);
 
 // console.log(faturamentoEmReais);
@@ -14,9 +14,41 @@ const faturamentoEmReais = faturamentoEmDolar.map(
 
 //filter 
 
-const faturamentoMaiorQueDesejado = faturamentoEmDolar.filter(
+const faturamentoMaiorQueDesejado = faturamento.filter(
   (valorFaturamentoMensal) => valorFaturamentoMensal >= 130_000
 );
 
 
-console.log(faturamentoMaiorQueDesejado);
+//console.log(faturamentoMaiorQueDesejado);
+
+
+
+// let faturamentoAnual = 0;
+// for (let i = 0; i < faturamento.length; i++) {
+//   faturamentoAnual += faturamento[i];
+// }
+
+// console.log(`O faturamento aunal foi de: ${faturamentoAnual}`);
+// console.log(`O faturamento medio do ultimo ano foi de: ${faturamentoAnual/faturamento.length}`);
+
+
+
+//reduce              parametros          funcao de callback
+const faturamentoAnual = faturamento.reduce((acumulador, elementoAtual) => acumulador + elementoAtual, 0);
+
+
+console.log(`O faturamento aunal foi de: ${faturamentoAnual}`);
+console.log(`O faturamento medio do ultimo ano foi de: ${faturamentoAnual/faturamento.length}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
