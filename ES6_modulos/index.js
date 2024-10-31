ab997e87083b9f3e531c92fe2176c963f33e0c95
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-10-31 11:24:14 -0300

    ESModules no node e a criação do package.json

---
+++ ES6_modulos/index.js
@@ -0,0 +1,27 @@
+//ES Modules, usar a extensao .mjs para O Node
+
+import { Car,  numberOfTires } from "./car.js";
+
+function createCars() {
+  const car1 = new Car('Toyota', 'Corolla', 2022);
+  const car2 = new Car('Toyota', 'Corolla', 2022);
+
+
+  car1.turnOn();
+  car2.turnOn();
+
+
+  console.log(car1.getDetails());
+  console.log(car2.getDetails());
+
+  car1.turnOff();
+  car1.turnOff();
+
+  console.log('Number',numberOfTires)
+}
+
+createCars();
+
+// const buttonElement = document.getElementById('run-fucntion');
+
+// buttonElement.addEventListener('click', createCars);
